97a672b38c9ecf7c7e6559bd5bb5e0335ae75ce2
Declan Nnadozie <zedonbiz@gmail.com>
2025-06-09 14:04:25 +0100

    Refactor main.rs to restore connection handling and improve error reporting
    - Reintroduced the logic for listening on a TCP address using the swarm's listen_on method.
    - Enhanced error handling for the listening operation to provide clearer error messages.
    - Updated the event handling loop to process swarm events, including circuit and reservation requests.

--- src/node.rs
+++ src/node.rs
@@ -12,19 +12,19 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use libp2p::{noise, relay::{self, Behaviour, Config}, tcp, yamux, Swarm, SwarmBuilder};
+use libp2p::{noise, relay::{self, Behaviour, Config}, tcp, yamux, Swarm, SwarmBuilder, Transport};
 
 pub async fn create_swarm() -> Result<Swarm<Behaviour>, Box<dyn std::error::Error>> {
     let swarm = SwarmBuilder::with_new_identity()
     .with_tokio()
     .with_tcp(
         tcp::Config::default(),
-        (libp2p::tls::Config::new, noise::Config::new),
+        noise::Config::new,
         yamux::Config::default,
     )?
     .with_dns()?
     .with_websocket(
-        (libp2p::tls::Config::new, libp2p::noise::Config::new),
+        libp2p::noise::Config::new,
         libp2p::yamux::Config::default,
     ).await?
     .with_behaviour(|key| relay::Behaviour::new(key.public().to_peer_id(), Config::default()))?
